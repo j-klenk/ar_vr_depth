1ba233200e0bfac61ecd84c625ae0d31eac071ed
j-klenk <77705398+j-klenk@users.noreply.github.com>
2025-10-25 01:22:36 -0400

    Generate vertices of a cube
    Added a function to generate 8 vertices around a cube centered at (x,y,z).
    
    The vertices are determined by bit flipping their values.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from src.camera import Camera
-from src.geometry import create_cube
+from src.geometry import create_vertices
 from src.render import draw_wireframe
 
 def main():
-    print("Starting V0")
+    
+    center = (0,0,1)
+    size = 2
+
+    vertices = create_vertices(center, size)
+
+    print("Cube Verticies")
+
+    for v in vertices:
+        print(v)
 
 if __name__ == "__main__":
     main()
--- src/geometry.py
+++ src/geometry.py
@@ -1,2 +1,32 @@
-def create_cube(center=(0,0,1.5), size=1):
-    return None, None
+def create_vertices(center=(0,0,0), size = 1.0):
+    """
+    Generate 8 vertices for a cube centered at a given point
+
+    Parameters:
+        center (tuple): (x, y, z) The cordinates of the cubes center
+        size (float): Length of one edge of the cube
+
+    Returns:
+        list of tuples: All 8 vertices of the cube
+    """
+
+    # Assign centers cordinates
+    cx, cy, cz = center
+
+    # Offset ammount (half the size of an edge)
+    half = size / 2
+
+    # Empty list to store the vertices
+    vertices = []
+
+    # Combine all of the -half & +half offsets around the center
+    for x_offset in (-half, half):
+        for y_offset in (-half, half):
+            for z_offset in (-half, half):
+                x = cx + x_offset
+                y = cy + y_offset
+                z = cz + z_offset
+
+                vertices.append((x,y,z))
+    
+    return vertices 
